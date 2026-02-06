e629d76e9fde386b9394c253009e373091843764
NANDAN POPAT <popatnandan21@gmail.com>
2026-02-06 22:56:18 +0530

    commit
    commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <h1>Auction Management System</h1>
+        <Routes>
+          {/* Define routes here */}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
