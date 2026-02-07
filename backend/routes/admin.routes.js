acd605fa88c4f71f3dfea5ab5fa7e9f77071c706
Darshan Jethava <jethvadarshan2006@gmail.com>
2026-02-07 06:50:32 +0530

    Admin
    All admin routes

---
+++ backend/routes/admin.routes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { registerAdmin, getAdmins, deleteAdmin, toggleAdminStatus } from "../controllers/auth.controller.js";
+import auth from "../middleware/auth.middleware.js";
+import authorize from "../middleware/role.middleware.js";
+
+const router = express.Router();
+
+router.post("/add", auth, authorize("MASTER_ADMIN"), registerAdmin);
+router.get("/", auth, authorize("MASTER_ADMIN"), getAdmins);
+router.delete("/:id", auth, authorize("MASTER_ADMIN"), deleteAdmin);
+router.put("/:id/status", auth, authorize("MASTER_ADMIN"), toggleAdminStatus);
+
+export default router;
