5ae1e6e34215c3e47d96da90fad4fe11bae4c61f
sneh557 <sneh7855@gmail.com>
2026-02-07 06:48:18 +0530

    Create bid.routes.js

---
+++ backend/routes/bid.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { placeBid, getAuctionState } from "../controllers/bid.controller.js";
+import auth from "../middleware/auth.middleware.js";
+import role from "../middleware/role.middleware.js";
+
+const router = express.Router();
+
+router.get("/:auctionId/state", getAuctionState);
+
+router.post("/place", auth, role(["TEAM", "ADMIN", "MASTER_ADMIN"]), placeBid);
+
+export default router;
