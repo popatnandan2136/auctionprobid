0629e02b47f8f4ea4733d8303062d392672f0659
sneh557 <sneh7855@gmail.com>
2026-02-07 06:30:12 +0530

    Stat Attribute

--- backend/controllers/skill.controller.js
+++ backend/controllers/skill.controller.js
@@ -5,8 +5,10 @@ export const createSkill = async (req, res) => {
     const skill = await Skill.create(req.body);
     res.json(skill);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).
+    json({ message: err.message });
   }
+  
 };
 
 export const getSkills = async (req, res) => {
---
+++ backend/controllers/statAttribute.controller.js
@@ -0,0 +1,42 @@
+import StatAttribute from "../models/StatAttribute.js";
+
+export const createStatAttribute = async (req, res) => {
+    try {
+        const { label, key, dataType } = req.body;
+        const stat = await StatAttribute.create({ label, key, dataType });
+        res.status(201).json({ message: "Stat Attribute created", stat });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const getAllStatAttributes = async (req, res) => {
+    try {
+        const stats = await StatAttribute.find().sort({ createdAt: -1 });
+        res.json(stats);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+export const updateStatAttribute = async (req, res) => {
+    try {
+        const { label, key, dataType } = req.body;
+        const stat = await StatAttribute.findByIdAndUpdate(
+            req.params.id,
+            { label, key, dataType },
+            { new: true }
+        );
+        res.json(stat);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const deleteStatAttribute = async (req, res) => {
+    try {
+        await StatAttribute.findByIdAndDelete(req.params.id);
+        res.json({ message: "Stat Attribute deleted" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
