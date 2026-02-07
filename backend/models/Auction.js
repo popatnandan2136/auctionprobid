fb734edcb41cf2c8a896f0bad175fdd983786c56
Darshan Jethava <jethvadarshan2006@gmail.com>
2026-02-07 06:20:14 +0530

    Updeted Auction
    Add more filed to essey to stor data

--- backend/models/Auction.js
+++ backend/models/Auction.js
@@ -1,28 +1,31 @@
 import mongoose from "mongoose";
 
-const auctionSchema = new mongoose.Schema(
-  {
+const auctionSchema = new mongoose.Schema({
+  name: String,
+  logoUrl: String,
+  pointsPerTeam: Number,
+  maxPlayersPerTeam: Number,
+  minPlayersPerTeam: Number,
+  incrementSteps: [Number],
+  auctionDate: Date,
+  totalTeams: Number,
+  currentPlayerId: { type: mongoose.Schema.Types.ObjectId, ref: "Player" },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  auctionType: { type: String, enum: ["WITH_STATS", "WITHOUT_STATS"] },
+  enabledStats: [{ key: String, label: String, dataType: String, required: Boolean }],
+  customCategories: [String], 
+  customRoles: [String], 
+  status: { type: String, enum: ["NOT_STARTED", "LIVE", "FINISHED"], default: "NOT_STARTED" },
+  isLive: { type: Boolean, default: false },
+  isRegistrationOpen: { type: Boolean, default: true },
+  lastBidTime: { type: Date, default: Date.now }, 
+  sponsors: [{
     name: String,
     logoUrl: String,
-
-    pointsPerTeam: Number,
-    maxPlayersPerTeam: Number,
-
-    auctionDate: Date,
-    totalTeams: Number,
-
-    status: {
-      type: String,
-      enum: ["NOT_STARTED", "LIVE", "FINISHED"],
-      default: "NOT_STARTED",
-    },
-
-    isLive: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  { timestamps: true }
-);
+    mobile: String,
+    address: String,
+    website: String
+  }]
+}, { timestamps: true });
 
 export default mongoose.model("Auction", auctionSchema);
